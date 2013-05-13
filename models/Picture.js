930e9bfd51472391f5b041fc4562bd01ea06bc28
jinjianfeng <wb-jinjianfeng@taobao.com>
2013-05-13 11:44:06 +0800

    edit

--- models/Picture.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var _Picture = new Schema({
-    versionId : Number,
-    path : String
-});
-
-exports.Picture = mongoose.model('picture', _Picture);
--- models/Project.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var _Project = new Schema({
-    name : String,
-    password : String
-});
-
-exports.Project = mongoose.model('projects', _Project);
